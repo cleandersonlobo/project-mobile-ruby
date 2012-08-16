e31320fa3cbb5a5b618d4ce7caf4adbf2d9fecc5
Cleanderson Lobo <cleandersonlobo@gmail.com>
2012-08-16 17:09:19 -0300

    Add files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectMobile::Application.config.secret_token = 'cc13ee305a40f0216140e3d1007522f061037e74fe923bd43e1c93e66e45cd7182116f5bdf7e7ece97f1d92a398b4d2f692cf0a02ae2b16716c0413e6626f6ab'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ProjectMobile::Application.config.session_store :cookie_store, key: '_project_mobile_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ProjectMobile::Application.config.session_store :active_record_store
